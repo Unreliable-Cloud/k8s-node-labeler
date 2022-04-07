64962e7f58f64717047c01875dfbf6158e2cfb29
Michael Beasley <michael.beasley@alvaria.com>
2022-04-07 13:12:11 -0500

    fix JSON for patches

--- docker/app.py
+++ docker/app.py
@@ -1,33 +1,31 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from time import sleep
 from kubernetes import client, config
-
+import json
 
 def main():
   config.load_incluster_config()
   api_instance = client.CoreV1Api()
 
-  workerConf = open("worker-labels.conf", "r")
-  spotConf = open("spot-labels.conf", "r")
+  spotConfig = 'spot-labels.conf'
+  workerConfig = 'worker-labels.conf'
 
-  worker = print(workerConf.read())
-  spot = print(spotConf.read())
+  with open(spotConfig) as f:
+    spot = json.loads(f.read())
 
-  while True:
+  with open(workerConfig) as f:
+    worker = json.loads(f.read())
 
-    spot_node_list = api_instance.list_node(label_selector="cloud.google.com/gke-spot=true")
-    for node in spot_node_list.items:
-        api_response = api_instance.patch_node(node.metadata.name, spot)
-        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
+  spot_node_list = api_instance.list_node(label_selector="cloud.google.com/gke-spot=true")
+  for node in spot_node_list.items:
+      api_response = api_instance.patch_node(node.metadata.name, body=spot)
+      print("%s\t%s" % (node.metadata.name, node.metadata.labels))
 
-    worker_node_list = api_instance.list_node()
-    for node in worker_node_list.items:
-        api_response = api_instance.patch_node(node.metadata.name, worker)
-        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
-
-    sleep(60)
-
+  worker_node_list = api_instance.list_node()
+  for node in worker_node_list.items:
+      api_response = api_instance.patch_node(node.metadata.name, body=worker)
+      print("%s\t%s" % (node.metadata.name, node.metadata.labels))
 
 if __name__ == '__main__':
   main()
