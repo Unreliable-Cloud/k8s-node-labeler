191147c59770b5ea5b8ff05537b0a290263315a7
Michael Beasley <michael.beasley@alvaria.com>
2022-04-07 02:50:59 -0500

    create chart

---
+++ docker/app.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+
+from time import sleep
+from kubernetes import client, config
+
+
+def main():
+  config.load_incluster_config()
+  api_instance = client.CoreV1Api()
+
+  worker = {
+    "metadata": {
+      "labels": {
+        "node-role.kubernetes.io/worker": ""
+      }
+    }
+  }
+
+  spot = {
+    "metadata": {
+      "labels": {
+        "node-role.kubernetes.io/spot-worker": ""
+      }
+    }
+  }
+
+  while True:
+
+    spot_node_list = api_instance.list_node(label_selector="spot_instance=true")
+    for node in spot_node_list.items:
+        api_response = api_instance.patch_node(node.metadata.name, spot)
+        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
+
+    worker_node_list = api_instance.list_node()
+    for node in worker_node_list.items:
+        api_response = api_instance.patch_node(node.metadata.name, worker)
+        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
+
+    sleep(60)
+
+
+if __name__ == '__main__':
+  main()
