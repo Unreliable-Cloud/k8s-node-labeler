ae84c827413ef6c72edc55d0a9478faca5f95953
Michael Beasley <michael.beasley@alvaria.com>
2022-04-09 15:01:57 -0500

    trivial

--- docker/app.py
+++ docker/app.py
@@ -15,23 +15,24 @@ def main():
   with open(workerConfig) as f:
     worker = json.loads(f.read())
 
-  try:
-    spot_node_list = api_instance.list_node(label_selector="cloud.google.com/gke-spot=true")
-    for node in spot_node_list.items:
-        api_response = api_instance.patch_node(node.metadata.name, body=spot)
-        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
-  except api_response.ApiException:
-    raise Exception("Unable to patch node")
+  while True:
+    try:
+      spot_node_list = api_instance.list_node(label_selector="cloud.google.com/gke-spot=true")
+      for node in spot_node_list.items:
+          api_response = api_instance.patch_node(node.metadata.name, body=spot)
+          print("%s\t%s" % (node.metadata.name, node.metadata.labels))
+    except api_response.ApiException:
+      raise Exception("Unable to patch node")
 
-  try:
-    worker_node_list = api_instance.list_node()
-    for node in worker_node_list.items:
-        api_response = api_instance.patch_node(node.metadata.name, body=worker)
-        print("%s\t%s" % (node.metadata.name, node.metadata.labels))
-  except api_response.ApiException:
-    raise Exception("Unable to patch node")
+    try:
+      worker_node_list = api_instance.list_node()
+      for node in worker_node_list.items:
+          api_response = api_instance.patch_node(node.metadata.name, body=worker)
+          print("%s\t%s" % (node.metadata.name, node.metadata.labels))
+    except api_response.ApiException:
+      raise Exception("Unable to patch node")
+
+    sleep(60)
 
 if __name__ == '__main__':
-  while True:
-    main()
-    sleep(60)
+  main()
