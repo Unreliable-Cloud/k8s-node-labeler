5a8b208e069ae2f4dcc5e77d471a2899719447a0
Michael Beasley <michael.beasley@alvaria.com>
2022-04-19 09:50:03 -0500

    add env vars for looking up node labels

--- docker/app.py
+++ docker/app.py
@@ -1,13 +1,16 @@
 from time import sleep
 from kubernetes import client, config
 import json
+import os
 
 def main():
   config.load_incluster_config()
   api_instance = client.CoreV1Api()
 
-  spotConfig = 'spot-labels.conf'
+  spotConfig   = 'spot-labels.conf'
   workerConfig = 'worker-labels.conf'
+  spotLabels   = os.environ.get('SPOTS')
+  workerLabels = os.environ.get('WORKERS')
 
   with open(spotConfig) as f:
     spot = json.loads(f.read())
@@ -17,7 +20,7 @@ def main():
 
   while True:
     try:
-      spot_node_list = api_instance.list_node(label_selector="cloud.google.com/gke-spot=true")
+      spot_node_list = api_instance.list_node(label_selector=spotLabels)
       for node in spot_node_list.items:
           api_response = api_instance.patch_node(node.metadata.name, body=spot)
           print("%s\t%s" % (node.metadata.name, node.metadata.labels))
@@ -25,7 +28,7 @@ def main():
       raise Exception("Unable to patch node")
 
     try:
-      worker_node_list = api_instance.list_node()
+      worker_node_list = api_instance.list_node(label_selector=workerLabels)
       for node in worker_node_list.items:
           api_response = api_instance.patch_node(node.metadata.name, body=worker)
           print("%s\t%s" % (node.metadata.name, node.metadata.labels))
