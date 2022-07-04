0dffaaa2db2ff32e6ffcdcd49ebcde013fbf6a81
Mike B <mike@donthurt.us>
2022-07-04 11:35:50 -0500

    revert leader election changes

--- docker/app.py
+++ docker/app.py
@@ -1,7 +1,5 @@
 from time import sleep
 from kubernetes import client, config
-from threading import Thread
-from leaderelection import Elect
 import json
 import yaml
 import os
@@ -12,13 +10,7 @@ def main():
   else:
     config.load_config()
 
-  api_instance   = client.CoreV1Api()
-  leaderelection = Elect(configmap='node-labeler-leader-election')
-  le             = Thread(target=leaderelection.run)
-  
-  le.setDaemon(True)
-  le.start()
-
+  api_instance = client.CoreV1Api()
   spotConfig   = 'spot-labels.yaml'
   workerConfig = 'worker-labels.yaml'
   spotLabels   = os.environ.get('SPOTS')
@@ -32,41 +24,31 @@ def main():
     worker = yaml.load(f.read(), Loader=yaml.FullLoader)
 
   while True:
-    leader = leaderelection.check_leader()
-    if leader:
-      logObject['message'] = "I am the leader with the lock"
-      print(json.dumps(logObject))
-      try:
-        spot_node_list = api_instance.list_node(label_selector=spotLabels)
-        for node in spot_node_list.items:
-            api_response               = api_instance.patch_node(node.metadata.name, body=spot)
-            node                       = node.metadata.name
-            logObject['nodeName']      = node
-            logObject['labelSelector'] = spotLabels
-            logObject['message']       = "Set spot-worker label for " + node
-            logOut                     = json.dumps(logObject)
-            print(logOut)
-      except api_response.ApiException:
-        raise Exception("Unable to patch node")
+    try:
+      spot_node_list = api_instance.list_node(label_selector=spotLabels)
+      for node in spot_node_list.items:
+          api_response               = api_instance.patch_node(node.metadata.name, body=spot)
+          node                       = node.metadata.name
+          logObject['nodeName']      = node
+          logObject['labelSelector'] = spotLabels
+          logObject['message']       = "Set spot-worker label for " + node
+          logOut                     = json.dumps(logObject)
+          print(logOut)
+    except api_response.ApiException:
+      raise Exception("Unable to patch node")
 
-      try:
-        worker_node_list = api_instance.list_node(label_selector=workerLabels)
-        for node in worker_node_list.items:
-            api_response               = api_instance.patch_node(node.metadata.name, body=worker)
-            node                       = node.metadata.name
-            logObject['nodeName']      = node
-            logObject['labelSelector'] = workerLabels
-            logObject['message']       = "Set worker label for " + node
-            logOut                     = json.dumps(logObject)
-            print(logOut)
-      except api_response.ApiException:
-        raise Exception("Unable to patch node")
-    else:
-      sleepTime = 10
-      logObject['message'] = "I am not the leader with the lock. Trying again in " + sleepTime + " seconds"
-      print(json.dumps(logObject))
-      sleep(sleepTime)
-
+    try:
+      worker_node_list = api_instance.list_node(label_selector=workerLabels)
+      for node in worker_node_list.items:
+          api_response               = api_instance.patch_node(node.metadata.name, body=worker)
+          node                       = node.metadata.name
+          logObject['nodeName']      = node
+          logObject['labelSelector'] = workerLabels
+          logObject['message']       = "Set worker label for " + node
+          logOut                     = json.dumps(logObject)
+          print(logOut)
+    except api_response.ApiException:
+      raise Exception("Unable to patch node")
     sleep(60)
 
 if __name__ == '__main__':
