753429dbb2a62284dda5e45623333e8bd42b9740
Mike B <mike@donthurt.us>
2022-07-04 15:55:33 -0500

    add leader election

--- docker/app.py
+++ docker/app.py
@@ -1,27 +1,55 @@
 from time import sleep
-from kubernetes import client, config
+import uuid
+from threading import Thread
 import json
 import yaml
 import os
+from kubernetes import client, config
+from kubernetes.leaderelection import leaderelection, electionconfig
+from kubernetes.leaderelection.resourcelock.configmaplock import ConfigMapLock
+
+if os.path.isdir("/var/run/secrets/kubernetes.io"):
+  config.load_incluster_config()
+  lock_namespace = os.environ.get('NAMESPACE')
+else:
+  config.load_config()
+  lock_namespace = "kube-system"
+
+api_instance   = client.CoreV1Api()
+spotConfig     = 'spot-labels.yaml'
+workerConfig   = 'worker-labels.yaml'
+spotLabels     = os.environ.get('SPOTS')
+workerLabels   = os.environ.get('WORKERS')
+candidate_id   = uuid.uuid4()
+lock_name      = "node-labeler-leader-election"
+logObject      = {}
 
 def main():
-  if os.path.isdir("/var/run/secrets/kubernetes.io"):
-    config.load_incluster_config()
-  else:
-    config.load_config()
+  leaseConfig    = electionconfig.Config(ConfigMapLock(lock_name, lock_namespace, candidate_id), lease_duration=15,
+                                         renew_deadline=10, retry_period=5, onstarted_leading=startLeader,
+                                         onstopped_leading=endLeader)
 
-  api_instance = client.CoreV1Api()
-  spotConfig   = 'spot-labels.yaml'
-  workerConfig = 'worker-labels.yaml'
-  spotLabels   = os.environ.get('SPOTS')
-  workerLabels = os.environ.get('WORKERS')
-  logObject    = {}
+  startElection = leaderelection.LeaderElection(leaseConfig)
+  th            = Thread(target=startElection.run())
+  th.setDaemon(True)
+  th.start()
+
+def endLeader():
+    logObject['info'] = "I not am the leader with the lock"
+    logOut            = json.dumps(logObject)
+    print(logOut)
+
+def startLeader():
+  logObject['info'] = "I am the leader with the lock"
+  logOut            = json.dumps(logObject)
 
   with open(spotConfig) as f:
     spot = yaml.load(f.read(), Loader=yaml.FullLoader)
 
   with open(workerConfig) as f:
     worker = yaml.load(f.read(), Loader=yaml.FullLoader)
+
+  print(logOut)
 
   while True:
     try:
